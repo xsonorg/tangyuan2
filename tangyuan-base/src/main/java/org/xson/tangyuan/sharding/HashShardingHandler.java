92c96119605e45ba78a256d3e74b735d3f28e7c4
Lenovo <Lenovo@DESKTOP-5OD6RFT>
2019-04-21 10:32:05 +0800

    update sharding.

--- tangyuan-base/src/main/java/org/xson/tangyuan/sharding/HashShardingHandler.java
+++ tangyuan-base/src/main/java/org/xson/tangyuan/sharding/HashShardingHandler.java
@@ -15,11 +15,39 @@ public class HashShardingHandler extends AbstractShardingHandler {
 		int hashCode = Math.abs(getHashCode(keywords[0], arg));
 
 		// 按照Hash划分, 1.hash mod dbCount, 2, hash mode talbeCount
-		long tableIndex = hashCode % defVo.getTableCount();
-		long dbIndex = hashCode % defVo.getDbCount();
-		if (defVo.isTableNameIndexIncrement()) {
-			tableIndex = tableIndex + (dbIndex * defVo.getTableCount());
+		// long tableIndex = hashCode % defVo.getTableCount();
+		// long dbIndex = hashCode % defVo.getDbCount();
+		// if (defVo.isTableNameIndexIncrement()) {
+		// tableIndex = tableIndex + (dbIndex * defVo.getTableCount());
+		// }
+
+		// fix bug with TableNameIndexIncrement
+		// long dbIndex = -1;
+		// long tableIndex = -1;
+		// if (defVo.isTableNameIndexIncrement()) {
+		// int allTable = defVo.getDbCount() * defVo.getTableCount();
+		// tableIndex = hashCode % allTable;
+		// dbIndex = tableIndex / defVo.getTableCount();
+		// } else {
+		// // 按照Hash划分, 1.hash mod dbCount, 2, hash mode talbeCount
+		// dbIndex = hashCode % defVo.getDbCount();
+		// tableIndex = hashCode % defVo.getTableCount();
+		// }
+		// if (-1 == dbIndex || -1 == tableIndex) {
+		// throw new ShardingException("分库分表索引不合法");
+		// }
+
+		// int allTable = defVo.getDbCount() * defVo.getTableCount();
+		// long dbIndex = hashCode % allTable;
+		// long tableIndex = tableIndex = hashCode % allTable;
+
+		int allTable = defVo.getDbCount() * defVo.getTableCount();
+		long tableIndex = hashCode % allTable;
+		long dbIndex = tableIndex / defVo.getTableCount();
+		if (!defVo.isTableNameIndexIncrement()) {
+			tableIndex = tableIndex - (dbIndex * defVo.getTableCount());
 		}
+
 		return getResult(tableIndex, dbIndex, defVo, argVo);
 	}
 
--- tangyuan-base/src/main/java/org/xson/tangyuan/sharding/RandomShardingHandler.java
+++ tangyuan-base/src/main/java/org/xson/tangyuan/sharding/RandomShardingHandler.java
@@ -5,11 +5,34 @@ public class RandomShardingHandler extends AbstractShardingHandler {
 	@Override
 	public ShardingResult selectDataSourceAndTable(ShardingDefVo defVo, ShardingArgVo argVo, Object arg) {
 		// 这种情况下不需要参数
-		long tableIndex = (int) (Math.random() * defVo.getTableCount());
-		int dbIndex = (int) (Math.random() * defVo.getDbCount());
-		if (defVo.isTableNameIndexIncrement()) {
-			tableIndex = tableIndex + (dbIndex * defVo.getTableCount());
+		// long tableIndex = (int) (Math.random() * defVo.getTableCount());
+		// int dbIndex = (int) (Math.random() * defVo.getDbCount());
+		// if (defVo.isTableNameIndexIncrement()) {
+		// tableIndex = tableIndex + (dbIndex * defVo.getTableCount());
+		// }
+
+		// long dbIndex = -1;
+		// long tableIndex = -1;
+		// if (defVo.isTableNameIndexIncrement()) {
+		// int allTable = defVo.getDbCount() * defVo.getTableCount();
+		// tableIndex = (int) (Math.random() * allTable);
+		// dbIndex = tableIndex / defVo.getTableCount();
+		// } else {
+		// dbIndex = (int) (Math.random() * defVo.getDbCount());
+		// tableIndex = (int) (Math.random() * defVo.getTableCount());
+		// }
+		//
+		// if (-1 == dbIndex || -1 == tableIndex) {
+		// throw new ShardingException("分库分表索引不合法");
+		// }
+
+		int allTable = defVo.getDbCount() * defVo.getTableCount();
+		long tableIndex = (int) (Math.random() * allTable);
+		long dbIndex = tableIndex / defVo.getTableCount();
+		if (!defVo.isTableNameIndexIncrement()) {
+			tableIndex = tableIndex - (dbIndex * defVo.getTableCount());
 		}
+
 		return getResult(tableIndex, dbIndex, defVo, argVo);
 	}
 
